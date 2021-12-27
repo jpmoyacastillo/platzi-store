cef7badd607fb7d81039cc077076c842fb726c00
jpmoya <juanpablo@rindegastos.com>
2021-12-27 20:22:20 +0100

    Done GET, POST, DELETE, EDIT to api

--- src/app/core/services/products/products.service.ts
+++ src/app/core/services/products/products.service.ts
@@ -1,16 +1,14 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient } from '@angular/common/http';
 
 import { Product } from '../../../products/product.model';
 
-import { environment } from "../../../../environments/environment";
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductsService {
-
-
   // products: Product[] = [
   //   {
   //     id: '1',
@@ -49,9 +47,7 @@ export class ProductsService {
   //   },
   // ];
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor(private http: HttpClient) {}
 
   getAllProducts() {
     return this.http.get<Product[]>(`${environment.url_api}/products/`);
@@ -59,5 +55,17 @@ export class ProductsService {
 
   getProduct(id: string) {
     return this.http.get<Product>(`${environment.url_api}/products/${id}`);
+  }
+
+  createProduct(product: Product) {
+    return this.http.post(`${environment.url_api}/products/`, product);
+  }
+
+  updateProduct(id: string, changes: Partial<Product>) {
+    return this.http.put(`${environment.url_api}/products/${id}`, changes);
+  }
+
+  deleteProduct(id: string) {
+    return this.http.delete(`${environment.url_api}/products/${id}`);
   }
 }
--- src/app/products/components/product-detail/product-detail.component.ts
+++ src/app/products/components/product-detail/product-detail.component.ts
@@ -4,36 +4,61 @@ import { ActivatedRoute, Params } from '@angular/router';
 import { ProductsService } from '../../../core/services/products/products.service';
 import { Product } from '../../product.model';
 
-
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.scss']
+  styleUrls: ['./product-detail.component.scss'],
 })
 export class ProductDetailComponent implements OnInit {
-
   product: Product;
 
   constructor(
     private route: ActivatedRoute,
     private productsService: ProductsService
-  ) {
-
-  }
+  ) {}
 
   ngOnInit(): void {
-   this.route.params.subscribe(( params: Params ) => {
-     const id = params['id'];
-     this.fetchProduct(id);
-     // this.product = this.productsService.getProduct(id);
-   });
+    this.route.params.subscribe((params: Params) => {
+      const id = params['id'];
+      this.fetchProduct(id);
+      // this.product = this.productsService.getProduct(id);
+    });
   }
 
-  fetchProduct(  id: string ) {
-    this.productsService.getProduct(id)
-    .subscribe( product => {
+  fetchProduct(id: string) {
+    this.productsService.getProduct(id).subscribe((product) => {
       this.product = product;
     });
   }
 
+  createProduct() {
+    const newProduct: Product = {
+      id: '222',
+      title: 'Nuevo desde Angular',
+      image: 'assets/images/banner-1.jpg',
+      price: 3000,
+      description: 'nuevo producto',
+    };
+    this.productsService.createProduct(newProduct).subscribe((product) => {
+      console.log(product);
+    });
+  }
+
+  updateProduct() {
+    const updateProduct: Partial<Product> = {
+      price: 55555,
+      description: 'edición título',
+    };
+    this.productsService
+      .updateProduct('2', updateProduct)
+      .subscribe((product) => {
+        console.log(product);
+      });
+  }
+
+  deleteProduct() {
+    this.productsService.deleteProduct('6').subscribe((rta) => {
+      console.log(rta);
+    });
+  }
 }
